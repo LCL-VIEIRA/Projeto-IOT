592fc781742c514d58c8810253b14752932f083d
LCL.V <lcl.vieira@hotmail.com>
2024-11-04 21:39:16 -0300

    Correção de Bug V2
    Resolução do problema em que o database não estava recebendo a string com a imagem em base64, apenas a timestamp

--- sketch_oct29a/sketch_oct29a.ino
+++ sketch_oct29a/sketch_oct29a.ino
@@ -2,15 +2,15 @@
 #include "esp_camera.h"
 #include <ArduinoJson.h>
 #include <HTTPClient.h>
-#include "Base64.h"  
+#include "Base64.h"
 #include "camera_pins.h"
 
 
-const char* ssid = "";
-const char* password = "";
+const char *ssid = "";
+const char *password = "";
 
-const String FIREBASE_URL = "https://firebasestorage.googleapis.com/v0/b/esp32-cam-31201.appspot.com/o/";
-const String API_KEY = "AIzaSyA__VKYgNW6Wbl0EALZ3pRfMr2Dva2FdrQ"; 
+const String FIREBASE_URL = "";
+const String API_KEY = "";
 
 void setup() {
   Serial.begin(115200);
@@ -20,7 +20,7 @@ void setup() {
 
 void loop() {
   sendImageToFirebase();
-  delay(30000); 
+  delay(30000);
 }
 
 void connectWiFi() {
@@ -54,31 +54,16 @@ void cameraInit() {
   config.pin_pwdn = PWDN_GPIO_NUM;
   config.pin_reset = RESET_GPIO_NUM;
   config.xclk_freq_hz = 20000000;
-  config.frame_size = FRAMESIZE_UXGA;
-  config.pixel_format = PIXFORMAT_JPEG; 
-  config.grab_mode = CAMERA_GRAB_WHEN_EMPTY;
+  config.frame_size = FRAMESIZE_XGA;
+  config.pixel_format = PIXFORMAT_JPEG;
+  config.grab_mode = CAMERA_GRAB_LATEST;
   config.fb_location = CAMERA_FB_IN_PSRAM;
-  config.jpeg_quality = 12;
-  config.fb_count = 1;
+  config.jpeg_quality = 10;
+  config.fb_count = 2;
 
-  if (config.pixel_format == PIXFORMAT_JPEG) {
-    if (psramFound()) {
-      config.jpeg_quality = 10;
-      config.fb_count = 2;
-      config.grab_mode = CAMERA_GRAB_LATEST;
-    } else {
-      config.frame_size = FRAMESIZE_SVGA;
-      config.fb_location = CAMERA_FB_IN_DRAM;
-    }
-  } else {
-    config.frame_size = FRAMESIZE_240X240;
-#if CONFIG_IDF_TARGET_ESP32S3
-    config.fb_count = 2;
-#endif
-  }
   if (esp_camera_init(&config) != ESP_OK) {
     Serial.println("Falha ao inicializar a câmera.");
-    while (true); 
+    while (true);
   }
   Serial.println("Câmera inicializada com sucesso!");
 }
@@ -95,11 +80,12 @@ void sendImageToFirebase() {
   }
 
   String imageBase64 = convertImageToBase64(fb);
-  esp_camera_fb_return(fb); 
+  esp_camera_fb_return(fb);
+  Serial.println ("Imagem em Base64: " + imageBase64);
 
-  StaticJsonDocument<50000> doc;
+  StaticJsonDocument<150000> doc;
   doc["timestamp"] = millis();
-  doc["image"] = imageBase64;
+  doc["image"] = "Imagem em Base64: " + imageBase64;
 
   String json;
   serializeJson(doc, json);
