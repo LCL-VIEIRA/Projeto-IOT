bf4feed23d98a139a2ef9dda698f6f2d1c587517
LCL.V <lcl.vieira@hotmail.com>
2024-11-07 23:41:13 -0300

    Implementação de horário
    usei as bibliotecas NTPClient e WifiUdp para sincronizar o horario no esp com o de um servidor e enviar no documento JSON junto com a imagem tirada.
    (tentei implementar data usando epoch mas a IDE não reconheceu de jeito nenhum suas funções)

--- sketch_oct29a/sketch_oct29a.ino
+++ sketch_oct29a/sketch_oct29a.ino
@@ -4,25 +4,34 @@
 #include <HTTPClient.h>
 #include "Base64.h"
 #include "camera_pins.h"
+#include <NTPClient.h>
+#include <WiFiUdp.h>
 
-
+//coloca o nome e a senha do wi-fi aqui
 const char *ssid = "";
 const char *password = "";
 
+//coloca a URL e a chave API do seu firebase aqui.
 const String FIREBASE_URL = "";
 const String API_KEY = "";
+
+WiFiUDP ntpUDP;
+NTPClient timeClient(ntpUDP, "pool.ntp.org", -10800, 60000);
 
 void setup() {
   Serial.begin(115200);
   connectWiFi();
   cameraInit();
+  timeClient.begin();
 }
 
+//chamada da função que envia imagem pro Firebase com o delay setado (30 segundos só que em milisegundos)
 void loop() {
   sendImageToFirebase();
   delay(30000);
 }
 
+//Função de conexão à internet
 void connectWiFi() {
   Serial.print("Conectando ao Wi-Fi");
   WiFi.begin(ssid, password);
@@ -33,6 +42,7 @@ void connectWiFi() {
   Serial.println("\nConectado ao Wi-Fi!");
 }
 
+//setagem de pinos do esp32 cam usando AI Thinker(extraí do exemplo pra ser mais fácil)
 void cameraInit() {
   camera_config_t config;
   config.ledc_channel = LEDC_CHANNEL_0;
@@ -54,13 +64,14 @@ void cameraInit() {
   config.pin_pwdn = PWDN_GPIO_NUM;
   config.pin_reset = RESET_GPIO_NUM;
   config.xclk_freq_hz = 20000000;
-  config.frame_size = FRAMESIZE_XGA;
+  config.frame_size = FRAMESIZE_VGA;
   config.pixel_format = PIXFORMAT_JPEG;
   config.grab_mode = CAMERA_GRAB_LATEST;
   config.fb_location = CAMERA_FB_IN_PSRAM;
   config.jpeg_quality = 10;
   config.fb_count = 2;
 
+  //teste de câmera
   if (esp_camera_init(&config) != ESP_OK) {
     Serial.println("Falha ao inicializar a câmera.");
     while (true);
@@ -68,6 +79,7 @@ void cameraInit() {
   Serial.println("Câmera inicializada com sucesso!");
 }
 
+//conversão da imagem para base64
 String convertImageToBase64(camera_fb_t *fb) {
   return base64::encode(fb->buf, fb->len);
 }
@@ -85,8 +97,9 @@ void sendImageToFirebase() {
 
   StaticJsonDocument<150000> doc;
   doc["timestamp"] = millis();
-  doc["image"] = "Imagem em Base64: " + imageBase64;
-
+  doc["imagembase64"] = imageBase64;
+  doc["datahora"] = getFormattedTime();
+  
   String json;
   serializeJson(doc, json);
 
@@ -95,6 +108,11 @@ void sendImageToFirebase() {
   } else {
     Serial.println("Erro ao enviar imagem.");
   }
+}
+
+String getFormattedTime() {
+  timeClient.update();
+  return timeClient.getFormattedTime();
 }
 
 bool sendDataToFirebase(const String &json) {
